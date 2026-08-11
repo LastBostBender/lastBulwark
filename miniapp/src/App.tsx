700e4b87b4637cb0d3ecf206323fcff644438db5
Last Bot Bender <microsporeisawesome@gmail.com>
2026-08-11 15:54:43 -0400

    Create App.tsx

---
+++ miniapp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Profile } from './pages/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
