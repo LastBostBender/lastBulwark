114a86c4bc089852e17d7ce000e70be4d6dc5823
Last Bot Bender <microsporeisawesome@gmail.com>
2026-08-14 10:19:18 -0400

    Update vite.config.ts

--- miniapp/vite.config.ts
+++ miniapp/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lastBulwark/',
   plugins: [react()],
   server: {
-    port: 3000, // Puerto para desarrollo local
-    open: true, // Abre el navegador automáticamente
+    port: 3000,
+    open: true,
   },
 });
